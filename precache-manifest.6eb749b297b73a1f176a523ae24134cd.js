878056c03f77c90c716d156af136364055ad0b8f
HEOS <heos20@gmail.com>
2020-08-13 16:27:49 -0500

    Updates

---
+++ precache-manifest.6eb749b297b73a1f176a523ae24134cd.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ebac1a61c27a33c11143137f871389",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "4dc3313eec83bfa201db",
+    "url": "/portafolio/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "07c7f179a4e1b513228a",
+    "url": "/portafolio/static/css/main.59742dbe.chunk.css"
+  },
+  {
+    "revision": "4dc3313eec83bfa201db",
+    "url": "/portafolio/static/js/2.347483e4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portafolio/static/js/2.347483e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07c7f179a4e1b513228a",
+    "url": "/portafolio/static/js/main.d303425a.chunk.js"
+  },
+  {
+    "revision": "0810d7b0182140f94756",
+    "url": "/portafolio/static/js/runtime-main.81433849.js"
+  },
+  {
+    "revision": "ee1d7b72ddd760bf05af37a50fd56c04",
+    "url": "/portafolio/static/media/bg-1.ee1d7b72.png"
+  },
+  {
+    "revision": "905e0ce1608471da8d1fe2ac296189a2",
+    "url": "/portafolio/static/media/carousel-1.905e0ce1.png"
+  },
+  {
+    "revision": "60f3a4d85c369521d2c39aaf886909f5",
+    "url": "/portafolio/static/media/carousel-2.60f3a4d8.png"
+  },
+  {
+    "revision": "32ca69b339a085fc393d0fd78591ead5",
+    "url": "/portafolio/static/media/carousel-3.32ca69b3.png"
+  },
+  {
+    "revision": "a5b0539f0b48b177339b08e1cf32a288",
+    "url": "/portafolio/static/media/logo-heos.a5b0539f.png"
+  },
+  {
+    "revision": "e0b66dff5c59e669d455c76ed58e20f7",
+    "url": "/portafolio/static/media/logo-proto.e0b66dff.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portafolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "31cee59667ed82b14d03486cbb96445d",
+    "url": "/portafolio/static/media/preview-messebau-virtual.31cee596.png"
+  },
+  {
+    "revision": "0e77cdbdf2fca773a8fcde791f4b5a23",
+    "url": "/portafolio/static/media/preview-messebau.0e77cdbd.png"
+  },
+  {
+    "revision": "bff5b00eb4b52d8d6432ff20cc17d265",
+    "url": "/portafolio/static/media/preview-skillup.bff5b00e.png"
+  }
+]);
